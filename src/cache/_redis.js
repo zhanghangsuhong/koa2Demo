9b92e23a6c93522f0c0edac95491a648513bb1f5
zhanghang <1170841058.com>
2020-12-07 19:32:45 +0800

    更新

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -3,33 +3,63 @@
  * @author zhang
  */
 
- const redis = require('redis')
- const { REDIS_CONF } = require('../conf/db')
+const redis = require('redis')
+const { REDIS_CONF } = require('../conf/db')
 
 
- const redisClient = redis.createClient(REDIS_CONF.port,REDIS_CONF.host)
- redisClient.on('error',err=>{
-	 console.log('redis error')
- })
-	 
-  /**
-	 * 
-	 * @param {string} key 
-	 * @param {string} val 
-	 * @param {number} timeout 过期时间
-	 */
-  function set(key,val,timeout = 60 * 60){
-		if(typeof val === 'object'){
-			val = JSON.stringify(val)
-		}
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+redisClient.on('error', err => {
+  console.log('redis error')
+})
 
-		redisClient.set(key,val)
-		redisClient.expire(key,timeout)
-	}
+/**
+ * redis set
+ * @param {string} key 
+ * @param {string} val 
+ * @param {number} timeout 过期时间
+ */
+function set(key, val, timeout = 60 * 60) {
+  if (typeof val === 'object') {
+    val = JSON.stringify(val)
+  }
 
-	module.exports = {
-		set
-	}
+  redisClient.set(key, val)
+  redisClient.expire(key, timeout)
+}
+
+/**
+ * redis get
+ * @param {string} key 
+ */
+function get(key) {
+  const promise = new Promise((resolve, reject) => {
+    redisClient.get(key, (err, val) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      if (val == null) {
+        resolve(null)
+        return
+      }
+
+      try {
+        resolve(JSON.parse(val))
+      } catch (ex) {
+        resolve(val)
+      }
+
+    })
+  })
+  return promise
+
+}
+
+module.exports = {
+  set,
+  get
+}
 
 
 
