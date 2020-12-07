db2cbafd450ea956ade2fa90eb2f5665156bed11
zhanghang <1170841058.com>
2020-12-07 16:10:27 +0800

    调整

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+
 const Koa = require('koa')
 const app = new Koa()
 const views = require('koa-views')
@@ -25,12 +26,12 @@ app.use(views(__dirname + '/views', {
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
